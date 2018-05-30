4e64572898a6ed7c90ce1b7a28609a21118f09d1
Apisit Toompakdee <apmotive@gmail.com>
2018-05-30 12:04:03 +0900

    make it a pointer

--- neoutils/nep2.go
+++ neoutils/nep2.go
@@ -2,8 +2,17 @@ package neoutils
 
 import "github.com/o3labs/neo-utils/neoutils/nep2"
 
-func NEP2Encrypt(wif string, passphrase string) (s string, address string, err error) {
-	return nep2.NEP2Encrypt(wif, passphrase)
+type NEP2 struct {
+	EncryptedKey string
+	Address      string
+}
+
+func NEP2Encrypt(wif string, passphrase string) (*NEP2, error) {
+	encryptedKey, address, err := nep2.NEP2Encrypt(wif, passphrase)
+	return &NEP2{
+		EncryptedKey: encryptedKey,
+		Address:      address,
+	}, err
 }
 
 func NEP2Decrypt(key, passphrase string) (s string, err error) {
